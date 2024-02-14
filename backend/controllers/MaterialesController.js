c2f9bd0addda6e94baa7094f8bd1ac4cd2610b8c
David Mora <mora_24_1@hotmail.com>
2024-02-14 08:45:53 -0600

    Correcion en los controladores para actualizar y eliminar algun material

--- backend/controllers/MaterialesController.js
+++ backend/controllers/MaterialesController.js
@@ -30,40 +30,44 @@ try {
         console.error(error);
         res.status(500).send({ message: 'Error interno del servidor' });
     }
+
 }
-           
 
 
-// const obtener_materiales_admin = async function(req, res) {
-// try {
-//     let materiales = await Materiales.find();
+const obtener_materiales_admin = async function(req, res) {
+    
+    var id = req.params['id']; 
 
-//     if (materiales.length > 0) {
-//         res.status(200).send({ data: materiales });
-//     } else {
-//         res.status(404).send({ message: 'No se encontraron materiales' });
-//     }
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).send({ message: 'Error interno del servidor' });
-//     }
-// }
-
-const listar_materiales_admin = async function (req,res){
-    try{
-        var filtro = req.params['filtro'];
-
-        let reg = await Materiales.find({titulo: new RegExp(filtro, 'i')});
-           
-            if(reg){
-                res.status(200).send({data: reg});//Asi lo mandamos al front end
-            }else{
-                res.status(500).send({message: 'Material no encontrado'});
-            }
+    try {
+        var reg = await Materiales.findById({_id:id}); 
+                
+        res.status(200).send({data:reg});
 
     } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: 'Error interno del servidor' });
+        res.status(200).send({data:undefined});
+    }
+
+     
+  
+
+
+}
+
+const listar_materiales_admin = async function (req,res){
+
+    if(req.user){
+        if(req.user.rol == 'admin'){
+            
+            var filtro = req.params['filtro'];
+
+            let reg = await Materiales.find({titulo: new RegExp(filtro, 'i')});
+            res.status(200).send({data: reg});
+
+        }else{
+            res.status(500).send({message: 'NoAccess'});
+        }
+    }else{
+        res.status(500).send({message: 'NoAccess'});
     }
 }
 
@@ -71,44 +75,49 @@ const listar_materiales_admin = async function (req,res){
 
 const actualizar_materiales_admin = async function(req, res) {
 try {
-    let titulo = req.params['titulo']; 
+    let id = req.params['id']; 
     
     let data = req.body;
     
-    let reg = await Materiales.findOneAndUpdate({ titulo: titulo }, {
+    let reg = await Materiales.findOneAndUpdate({_id : id }, {
+    titulo : data.titulo, 
     categoria: data.categoria,
     stock: data.stock,
     estado: data.estado,
-    }, { new: true });
+});
+    res.status(200).send({data:reg});
     
-    if (reg) {
-        res.status(200).send({ data: reg });
-    } else {
-        res.status(404).send({ message: 'Material no encontrado' });
-    }
+
     } catch (error) {
         console.error(error);
         res.status(500).send({ message: 'Error interno del servidor' });
     }
+
 }
+
     
     
 const eliminar_materiales_admin = async function(req, res) {
-try {
-    let titulo = req.params['titulo']; 
+
+if(req.user){
+    if(req.user.rol =='admin'){
+        
+      
+       var id = req.params['id']; 
+       
+       let reg = await Materiales.findOneAndDelete({_id:id});
+        res.status(200).send({data:reg}); 
+  
+  
+    }else{
+        res.status(500).send({message: 'NoAccess'});
     
-    let reg = await Materiales.findOneAndDelete({ titulo: titulo });
-    
-    if (reg) {
-        res.status(200).send({ message: 'Material eliminado correctamente', data: reg });
-    } else {
-        res.status(404).send({ message: 'Material no encontrado' });
     }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: 'Error interno del servidor' });
-    }
+}else{
+        res.status(500).send({message: 'NoAccess'});
     
+
+}
 }
     
 const listar_materiales_categoria = async function(req,res){
@@ -134,7 +143,7 @@ const listar_materiales_categoria = async function(req,res){
 module.exports = {
 registro_materiales_admin,
 listar_materiales_admin,
-// obtener_materiales_admin,
+obtener_materiales_admin,
 actualizar_materiales_admin,
 eliminar_materiales_admin,
 listar_materiales_categoria,
--- backend/routes/materiales.js
+++ backend/routes/materiales.js
@@ -10,10 +10,10 @@ var api = express.Router();
 
 
 api.post('/registro_materiales_admin', auth.auth,materialesController.registro_materiales_admin); 
-// api.get('/obtener_materiales_admin/', materialesController.obtener_materiales_admin);
+api.get('/obtener_materiales_admin/:id',auth.auth ,  materialesController.obtener_materiales_admin);
 api.get('/listar_materiales_admin/:filtro?',auth.auth, materialesController.listar_materiales_admin);
-api.put('/actualizar_materiales_admin/',auth.auth, materialesController.actualizar_materiales_admin); 
-api.delete('/eliminar_materiales_admin/',auth.auth, materialesController.eliminar_materiales_admin);  
+api.put('/actualizar_materiales_admin/:id',auth.auth, materialesController.actualizar_materiales_admin); 
+api.delete('/eliminar_materiales_admin/:id',auth.auth, materialesController.eliminar_materiales_admin);  
 
 api.post('/registro_categoria_admin', categoriaController.registro_categoria_admin);
 api.delete('/eliminar_categoria_admin/:id', categoriaController.eliminar_categoria_admin);
